b8cf2cc4e57ab566693d631c04c6a8316ec4a5c6
Stephen Hodgson <hodgson.designs@gmail.com>
2022-12-10 17:59:10 -0500

    a whole bunch of refactoring

--- dist/index.js
+++ dist/index.js
@@ -6716,60 +6716,61 @@ const core = __nccwpck_require__(2186);
 const tc = __nccwpck_require__(7784);
 const exec = __nccwpck_require__(1514);
 
+const ovrPlatformUtil = 'ovr-platform-util';
+const IS_WINDOWS = process.platform === 'win32'
+const IS_MAC = process.platform === 'darwin'
+
 const main = async () => {
     try {
+        const fileEx = IS_WINDOWS ? '.exe' : '';
         let osPlatform = os.platform();
-        let pathToCLI = tc.find('ovr-platform-util', '1.0.0');
+        let pathToModule = undefined;
+        let pathToToolDir = tc.find(ovrPlatformUtil, '1.0.0');
 
-        if (!pathToCLI) {
+        if (!pathToToolDir) {
             let url = undefined;
             let downloadPath = undefined;
-            let fileEx = '';
 
-            if (osPlatform == 'darwin') {
+            if (IS_MAC) {
                 url = 'https://www.oculus.com/download_app/?id=1462426033810370';
-            } else if (osPlatform == 'win32') {
+            } else if (IS_WINDOWS) {
                 url = 'https://www.oculus.com/download_app/?id=1076686279105243';
-                fileEx = '.exe';
             } else {
-                throw Error(`ovr-platform-util not available for ${osPlatform}`);
+                throw Error(`${ovrPlatformUtil} not available for ${osPlatform}`);
             }
 
-            core.info(`Attempting to download ovr-platform-util from ${url}`);
+            let fileName =  `${ovrPlatformUtil}${fileEx}`;
+            downloadPath = path.resolve(getTempDirectory(), fileName);
+
+            core.info(`Attempting to download ${ovrPlatformUtil} from ${url} to ${downloadPath}`);
 
             try {
-                downloadPath = await tc.downloadTool(url);
+                downloadPath = await tc.downloadTool(url, downloadPath);
             } catch (error) {
                 throw error;
             }
 
-            core.info(`Successfully downloaded ovr-platform-util to ${downloadPath}`);
+            core.info(`Successfully downloaded ${ovrPlatformUtil} to ${downloadPath}`);
+            core.info(`Setting tool cache ${downloadPath} | ${fileName} | ${ovrPlatformUtil}`);
 
-            let fileName =  `ovr-platform-util${fileEx}`;
-            core.info(`Setting tool cache ${downloadPath} | ${fileName} | ovr-platform-util`);
-            pathToCLI = await tc.cacheFile(downloadPath, fileName, 'ovr-platform-util', '1.0.0');
-            core.info(`pathToCLI: ${pathToCLI}`);
-
-            const files = await readdir(pathToCLI);
-
-            for (const file of files) {
-                const item = path.resolve(pathToCLI, file);
-                core.info(item);
-            }
+            pathToToolDir = await tc.cacheFile(downloadPath, fileName, ovrPlatformUtil, '1.0.0');
+            pathToModule = getExecutable(pathToToolDir);
 
             if (osPlatform == 'darwin') {
                 core.info(`Change the access permissions of the utility for it to execute`);
-                exec.exec(`chmod +x ${pathToCLI}`);
+                exec.exec(`chmod +x ${pathToModule}`);
             }
         } else {
-            exec.exec(pathToCLI, 'self-update');
+            pathToModule = getExecutable(pathToToolDir);
+            exec.exec(pathToModule, 'self-update');
         }
 
-        core.addPath(pathToCLI);
-        core.exportVariable('ovr-platform-util', pathToCLI);
+        core.info(`pathToToolDir: ${pathToToolDir}`);
+        core.addPath(pathToToolDir);
+        core.exportVariable(ovrPlatformUtil, pathToModule);
 
-        exec.exec('ovr-platform-util', 'version');
-        exec.exec('ovr-platform-util', 'help');
+        exec.exec(ovrPlatformUtil, 'version');
+        exec.exec(ovrPlatformUtil, 'help');
     } catch (error) {
         core.setFailed(error);
     }
@@ -6777,9 +6778,15 @@ const main = async () => {
 
 main();
 
-function _getTempDirectory() {
+function getTempDirectory() {
     const tempDirectory = process.env['RUNNER_TEMP'] || ''
     return tempDirectory
+}
+
+function getExecutable(dir) {
+    const fileEx = IS_WINDOWS ? '.exe' : '';
+    const moduleName = `${ovrPlatformUtil}${fileEx}`;
+    return path.resolve(dir, moduleName);
 }
 })();
 
--- src/index.js
+++ src/index.js
@@ -5,23 +5,26 @@ const path = require('path');
 const core = require('@actions/core');
 const tc = require('@actions/tool-cache');
 const exec = require('@actions/exec');
+
 const ovrPlatformUtil = 'ovr-platform-util';
+const IS_WINDOWS = process.platform === 'win32'
+const IS_MAC = process.platform === 'darwin'
 
 const main = async () => {
     try {
+        const fileEx = IS_WINDOWS ? '.exe' : '';
         let osPlatform = os.platform();
-        let pathToCLI = tc.find(ovrPlatformUtil, '1.0.0');
+        let pathToModule = undefined;
+        let pathToToolDir = tc.find(ovrPlatformUtil, '1.0.0');
 
-        if (!pathToCLI) {
+        if (!pathToToolDir) {
             let url = undefined;
             let downloadPath = undefined;
-            let fileEx = '';
 
-            if (osPlatform == 'darwin') {
+            if (IS_MAC) {
                 url = 'https://www.oculus.com/download_app/?id=1462426033810370';
-            } else if (osPlatform == 'win32') {
+            } else if (IS_WINDOWS) {
                 url = 'https://www.oculus.com/download_app/?id=1076686279105243';
-                fileEx = '.exe';
             } else {
                 throw Error(`${ovrPlatformUtil} not available for ${osPlatform}`);
             }
@@ -38,28 +41,23 @@ const main = async () => {
             }
 
             core.info(`Successfully downloaded ${ovrPlatformUtil} to ${downloadPath}`);
-
             core.info(`Setting tool cache ${downloadPath} | ${fileName} | ${ovrPlatformUtil}`);
-            pathToCLI = await tc.cacheFile(downloadPath, fileName, ovrPlatformUtil, '1.0.0');
-            core.info(`pathToCLI: ${pathToCLI}`);
 
-            const files = await readdir(pathToCLI);
-
-            for (const file of files) {
-                const item = path.resolve(pathToCLI, file);
-                core.info(item);
-            }
+            pathToToolDir = await tc.cacheFile(downloadPath, fileName, ovrPlatformUtil, '1.0.0');
+            pathToModule = getExecutable(pathToToolDir);
 
             if (osPlatform == 'darwin') {
                 core.info(`Change the access permissions of the utility for it to execute`);
-                exec.exec(`chmod +x ${pathToCLI}`);
+                exec.exec(`chmod +x ${pathToModule}`);
             }
         } else {
-            exec.exec(pathToCLI, 'self-update');
+            pathToModule = getExecutable(pathToToolDir);
+            exec.exec(pathToModule, 'self-update');
         }
 
-        core.addPath(pathToCLI);
-        core.exportVariable(ovrPlatformUtil, pathToCLI);
+        core.info(`pathToToolDir: ${pathToToolDir}`);
+        core.addPath(pathToToolDir);
+        core.exportVariable(ovrPlatformUtil, pathToModule);
 
         exec.exec(ovrPlatformUtil, 'version');
         exec.exec(ovrPlatformUtil, 'help');
@@ -73,4 +71,10 @@ main();
 function getTempDirectory() {
     const tempDirectory = process.env['RUNNER_TEMP'] || ''
     return tempDirectory
+}
+
+function getExecutable(dir) {
+    const fileEx = IS_WINDOWS ? '.exe' : '';
+    const moduleName = `${ovrPlatformUtil}${fileEx}`;
+    return path.resolve(dir, moduleName);
 }
