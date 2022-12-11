1a6ceab4a2b7ab64984fa9f5cc2923d9060cb5d5
Stephen Hodgson <hodgson.designs@gmail.com>
2022-12-10 19:00:43 -0500

    update pattern

--- dist/index.js
+++ dist/index.js
@@ -9903,7 +9903,7 @@ const main = async () => {
             };
 
             await exec.exec(downloadPath, 'version', options);
-            const semVerRegEx = new RegExp(/^([0-9]+)\.([0-9]+)\.([0-9]+)(?:-([0-9A-Za-z-]+(?:\.[0-9A-Za-z-]+)*))?(?:\+[0-9A-Za-z-]+)?/);
+            const semVerRegEx = new RegExp(/([0-9]+)\.([0-9]+)\.([0-9]+)(?:-([0-9A-Za-z-]+(?:\.[0-9A-Za-z-]+)*))?(?:\+[0-9A-Za-z-]+)?/);
             const matches = output.match(semVerRegEx);
             core.info(`matches: ${matches}`);
             let downloadedVersion = semver.clean(matches[0]);
